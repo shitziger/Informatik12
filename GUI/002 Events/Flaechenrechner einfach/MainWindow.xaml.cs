b5465e5652c8508d733de6702ed33a85fdd898b3
Sebastian Hitziger <115802442+Borstolus@users.noreply.github.com>
2025-02-18 10:39:26 +0100

    flaechenrechner added

---
+++ GUI/002 Events/Flaechenrechner einfach/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flaechenrechner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btBerechne_Click(object sender, RoutedEventArgs e)
+        {
+            // Einlesen
+            double laenge = double.Parse(tbLaenge.Text);
+            double breite = double.Parse(tbBreite.Text);
+
+            // Berechnen
+            double flaeche = laenge * breite;
+
+            // Ausgabe
+            lbAusgabe.Content = $"Die Fläche beträgt {flaeche}.";
+
+        }
+    }
+}
